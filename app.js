88d1fffcd9766ed972d67a1b3161eff9c3a1bbc2
abyyazhari-hub <abyyazhari@gmail.com>
2026-08-06 08:48:05 +0700

    Update app.js

--- app.js
+++ app.js
@@ -240,3 +240,51 @@ END
 ========================== */
 
 console.log("RAZHPOINT Ready 🚀");
+
+/* ==========================
+SLIDER
+========================== */
+
+const slides=document.querySelectorAll(".slide");
+
+const dots=document.querySelectorAll(".dot");
+
+let currentSlide=0;
+
+function showSlide(index){
+
+slides.forEach(s=>s.classList.remove("active"));
+
+dots.forEach(d=>d.classList.remove("active"));
+
+slides[index].classList.add("active");
+
+dots[index].classList.add("active");
+
+}
+
+setInterval(()=>{
+
+currentSlide++;
+
+if(currentSlide>=slides.length){
+
+currentSlide=0;
+
+}
+
+showSlide(currentSlide);
+
+},5000);
+
+dots.forEach((dot,index)=>{
+
+dot.onclick=()=>{
+
+currentSlide=index;
+
+showSlide(index);
+
+};
+
+});
